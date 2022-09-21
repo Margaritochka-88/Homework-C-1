6ebbf8a5cb1da5b4418c058e9a04318b85713bbb
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 13:53:08 +0300

    002

--- Test/002/Program.cs
+++ Test/002/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+
+
+if (numberA > numberB) 
+{
+    Console.WriteLine($" max = {numberA}");
+    Console.WriteLine($" min = {numberB}");
+}
+else if (numberA < numberB) 
+{
+    Console.WriteLine($" max = {numberB}");
+    Console.WriteLine($" min = {numberA}");
+}
+else {
+    Console.WriteLine("Числа равны ");
+    
+}

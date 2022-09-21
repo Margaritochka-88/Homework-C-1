bca3dbcdccbdeae5050b5c9a4784307efab5a2fd
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 17:15:11 +0300

    013(1)

--- Test/013/Program.cs
+++ Test/013/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write( "Ведите число: ");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write( "Ведите число: ");
 int Number =Convert.ToInt32(Console.ReadLine());
 string NumberText=Convert.ToString(Number);
 

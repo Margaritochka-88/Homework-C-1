12b30726a66353600f010f6e136a0b3e428d301a
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 20:10:03 +0300

    015(1)

--- Test/015/Program.cs
+++ Test/015/Program.cs
@@ -1,34 +1,43 @@
-﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
+﻿// // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// // 6 -> да
+// // 7 -> да
+// // 1 -> нет
 
 
-Console.WriteLine("Введите номер дня недели: ");
-string numberStr = Console.ReadLine();
+// Console.WriteLine("Введите номер дня недели: ");
+// string numberStr = Console.ReadLine();
 
-if (numberStr =="1") {
-    Console.WriteLine("-> нет");
-}
-else if (numberStr =="2") {
-    Console.WriteLine("-> нет");
-}
-else if (numberStr =="3") {
-    Console.WriteLine("-> нет");
-}
-else if (numberStr =="4") {
-    Console.WriteLine("-> нет");
-}
-else if (numberStr =="5") {
-    Console.WriteLine("-> нет");
-}
-else if (numberStr =="6") {
-    Console.WriteLine("-> да");
-}
-else if (numberStr =="7") {
-    Console.WriteLine("-> да");
-}
+// if (numberStr =="1") {
+//     Console.WriteLine("-> нет");
+// }
+// else if (numberStr =="2") {
+//     Console.WriteLine("-> нет");
+// }
+// else if (numberStr =="3") {
+//     Console.WriteLine("-> нет");
+// }
+// else if (numberStr =="4") {
+//     Console.WriteLine("-> нет");
+// }
+// else if (numberStr =="5") {
+//     Console.WriteLine("-> нет");
+// }
+// else if (numberStr =="6") {
+//     Console.WriteLine("-> да");
+// }
+// else if (numberStr =="7") {
+//     Console.WriteLine("-> да");
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели не существует! ");
+// }
+
+
+int number = int.Parse(numberStr);
+if(number >=1 && number <= 5)
+Console.WriteLine("нет");
+else if(number == 6 || number == 7)
+Console.WriteLine("да");
 else
-{
-    Console.WriteLine("Такого дня недели не существует! ");
-}
+Console.WriteLine("Такого дня недели не существует! ");

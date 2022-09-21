86057c53a7acd6c978c77ba7a653744cd07447de
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 15:48:05 +0300

    004

---
+++ Test/004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = int.Parse(Console.ReadLine());
+
+int max= numberA;
+
+if (numberB > numberA) max=numberB;
+if (numberC > numberA) max=numberC;
+
+Console.WriteLine(max);

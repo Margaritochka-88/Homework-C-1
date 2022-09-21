2190558ea2a5276cc044b0993fb3ee168bc9772b
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 17:15:33 +0300

    010

---
+++ Test/010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindeNewDigit(int num)
+{
+    int result = num % 100 / 10;
+    return result;
+}
+int res = FindeNewDigit(number);
+Console.WriteLine(res);

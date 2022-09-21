503ce9e369ff1b06ff024472649e63a806bf50f3
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-21 16:03:03 +0300

    006

---
+++ Test/006/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Введёное число является чётным");
+    Console.ReadLine();
+}
+else 
+{
+    Console.WriteLine("Введёное число является нечётным");
+    Console.ReadLine();
+}
+
+
+
+

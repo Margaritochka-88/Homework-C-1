482df7a867fdd694c3e3c499d547a0b6a6d55693
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-28 00:39:14 +0300

    023

--- Test/023/Program.cs
+++ Test/023/Program.cs
@@ -1,4 +1,8 @@
-﻿int ReadData(string line)
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int ReadData(string line)
 {
 Console.WriteLine(line);
 int number = int.Parse(Console.ReadLine() ?? "");

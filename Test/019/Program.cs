d3f37e6b77aefddd5b4a7bb81436f270d6a91804
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-28 00:55:26 +0300

    019

---
+++ Test/019/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int ReadData(string line)
+{
+Console.WriteLine(line);
+int number = int.Parse(Console.ReadLine() ?? "");
+return number;
+}
+
+void PrintData(string prefix, string value)
+
+{
+Console.WriteLine(prefix + value);
+}
+string PolyTest (int num)
+{
+    if(num/10000==num%10 && (num/1000)%10==(num/10)%10)
+    return "Число полиндром";
+    else
+    {
+        return" Число не полиндром";
+    }
+}
+
+int number = ReadData ("Введите пятизначное число");
+if(number>=10000 && number < 100000)
+{
+    PrintData(PolyTest(number), "");
+}
+else
+{
+    PrintData("вы ввели не пятизначное число","");
+}

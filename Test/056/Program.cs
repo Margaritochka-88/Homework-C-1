8f935f956bd476a14d317f6134655c391ff9fef9
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-10-17 23:32:42 +0300

    056

---
+++ Test/056/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Задача 56: 
+// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка
+
+
+
+
+
+int[,] GenerateIntRndMatrix(int rows, int columns)
+{
+    int[,] inputMatr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            inputMatr[i, j] = rnd.Next(0, 11);
+    }
+    return inputMatr;
+}
+
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+            Console.Write($"{inputMatrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+int inputMatrSum(int ind, int[,] inputMatr)
+{
+    int sum = 0;
+    for (int i = 0; i < inputMatr.GetLength(1); i++)
+    {
+        sum = sum + inputMatr[ind, i];
+    }
+    return sum;
+}
+
+int MinimumOfVector(int[] vec)
+{
+    int min = vec[0];
+    int minIndex = 0;
+    for (int i = 0; i < vec.Length; i++)
+    {
+        if (vec[i] < min)
+        {
+            min = vec[i];
+            minIndex = i;
+        }
+    }
+    return minIndex + 1;
+}
+
+int MinimalInd(int[,] matrix)
+{
+    int[] vectorOfSums = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        vectorOfSums[i] = inputMatrSum(i, matrix);
+    return MinimumOfVector(vectorOfSums);
+}
+
+int height = 3;
+int width = 4;
+int[,] testMatrix = GenerateIntRndMatrix(height, width);
+PrintMatrix(testMatrix);
+Console.WriteLine();
+int min = MinimalInd(testMatrix);
+Console.WriteLine($" Индекс строки сумма элементов которой минимальна => {min} ");
